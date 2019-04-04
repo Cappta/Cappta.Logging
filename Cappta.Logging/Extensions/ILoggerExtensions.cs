5c485a485d8faebee3109fb3778b9af740f86926
Sérgio Fonseca <sergio.fonseca@cappta.com.br>
2019-04-04 15:35:26 -0300

    Added extensions methods for exception logging without a message

---
+++ Cappta.Logging/Extensions/ILoggerExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace Cappta.Logging.Extensions
+{
+	public static class ILoggerExtensions
+	{
+		public static void Log(this ILogger logger, LogLevel logLevel, Exception exception)
+			=> logger.Log(logLevel, exception, string.Empty);
+
+		public static void Log(this ILogger logger, LogLevel logLevel, EventId eventId, Exception exception)
+			=> logger.Log(logLevel, eventId, exception, string.Empty);
+
+		public static void LogCritical(this ILogger logger, Exception exception)
+			=> logger.LogCritical(exception, string.Empty);
+
+		public static void LogCritical(this ILogger logger, EventId eventId, Exception exception)
+			=> logger.LogCritical(eventId, exception, string.Empty);
+
+		public static void LogDebug(this ILogger logger, Exception exception)
+			=> logger.LogDebug(exception, string.Empty);
+
+		public static void LogDebug(this ILogger logger, EventId eventId, Exception exception)
+			=> logger.LogDebug(eventId, exception, string.Empty);
+
+		public static void LogError(this ILogger logger, Exception exception)
+			=> logger.LogError(exception, string.Empty);
+
+		public static void LogError(this ILogger logger, EventId eventId, Exception exception)
+			=> logger.LogError(eventId, exception, string.Empty);
+
+		public static void LogInformation(this ILogger logger, Exception exception)
+			=> logger.LogInformation(exception, string.Empty);
+
+		public static void LogInformation(this ILogger logger, EventId eventId, Exception exception)
+			=> logger.LogInformation(eventId, exception, string.Empty);
+
+		public static void LogTrace(this ILogger logger, Exception exception)
+			=> logger.LogTrace(exception, string.Empty);
+
+		public static void LogTrace(this ILogger logger, EventId eventId, Exception exception)
+			=> logger.LogTrace(eventId, exception, string.Empty);
+
+		public static void LogWarning(this ILogger logger, Exception exception)
+			=> logger.LogWarning(exception, string.Empty);
+
+		public static void LogWarning(this ILogger logger, EventId eventId, Exception exception)
+			=> logger.LogWarning(eventId, exception, string.Empty);
+	}
+}
