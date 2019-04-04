a89f41c014e082d80c546a8625baf15be10747b0
Sérgio Fonseca <sergio.fonseca@cappta.com.br>
2019-04-04 14:54:06 -0300

    Changed AsyncLogService to work with sync worker threads

--- Cappta.Logging/Services/AsyncLogService.cs
+++ Cappta.Logging/Services/AsyncLogService.cs
@@ -2,14 +2,13 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 
 namespace Cappta.Logging.Services
 {
 	public class AsyncLogService : ILogService, IDisposable
 	{
-		private const int DEFAULT_BATCH_LIMIT = 10;
+		private const int DEFAULT_SYNC_JOBS = 10;
 		private const int DEFAULT_SIZE_LIMIT = 10000;
 
 		private static readonly TimeSpan IDLE_SLEEP_TIME = TimeSpan.FromMilliseconds(100);
@@ -19,31 +18,27 @@ namespace Cappta.Logging.Services
 
 		private readonly ConcurrentQueue<JsonLog> jsonLogQueue = new ConcurrentQueue<JsonLog>();
 		private readonly ILogService logService;
-		private readonly int batchLimit;
 		private readonly int sizeLimit;
 
 		private int lostLogCounter = 0;
 		private bool disposing = false;
 
-		public AsyncLogService(ILogService logService, int batchLimit = DEFAULT_BATCH_LIMIT, int sizeLimit = DEFAULT_SIZE_LIMIT)
+		public AsyncLogService(ILogService logService, int syncJobs = DEFAULT_SYNC_JOBS, int sizeLimit = DEFAULT_SIZE_LIMIT)
 		{
 			this.logService = logService ?? throw new ArgumentNullException(nameof(logService));
-			this.batchLimit = batchLimit;
 			this.sizeLimit = sizeLimit;
 
-			var thread = new Thread(this.IndexingThreadFunc);
-			thread.Name = $"{nameof(AsyncLogService)}({logService.GetType().Name})"; //this helps identify this thread when debugging
-			thread.IsBackground = true; //this means that the program can close with this thread running
-			thread.Start();
+			for (var i = 0; i < syncJobs; i++)
+			{
+				var thread = new Thread(this.IndexingThreadFunc);
+				thread.Name = $"{nameof(AsyncLogService)}({logService.GetType().Name}) #{i}"; //this helps identify this thread when debugging
+				thread.IsBackground = true; //this means that the program can close with this thread running
+				thread.Start();
+			}
 		}
 
 		public void Log(IDictionary<string, object> data)
 			=> this.Log(new JsonLog(data));
-
-		public void Log(IEnumerable<JsonLog> jsonLogs)
-		{
-			foreach (var jsonLog in jsonLogs) { this.Log(jsonLog); }
-		}
 
 		public void Log(JsonLog jsonLog)
 		{
@@ -62,28 +57,17 @@ namespace Cappta.Logging.Services
 		{
 			while (this.disposing == false)
 			{
-				var jsonLogBatch = this.EnumerateNextJsonLogBatch().ToArray();
-
-				if (jsonLogBatch.Length == 0) { Thread.Sleep(IDLE_SLEEP_TIME); continue; }
+				if (this.jsonLogQueue.TryDequeue(out var jsonLog) == false) { Thread.Sleep(IDLE_SLEEP_TIME); continue; }
 
 				try
 				{
-					this.logService.Log(jsonLogBatch);
+					this.logService.Log(jsonLog);
 				}
 				catch (Exception ex)
 				{
-					this.Log(jsonLogBatch);
+					this.Log(jsonLog);
 					this.Exception?.Invoke(ex);
 				}
-			}
-		}
-
-		private IEnumerable<JsonLog> EnumerateNextJsonLogBatch()
-		{
-			for (var i = 0; i < this.batchLimit; i++)
-			{
-				if (this.jsonLogQueue.TryDequeue(out var jsonLog) == false) { yield break; }
-				yield return jsonLog;
 			}
 		}
 
