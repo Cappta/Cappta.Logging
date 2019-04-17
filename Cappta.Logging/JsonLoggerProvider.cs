531e734d9e3e4316c723b1c21c4aa1ce647c19f9
Sérgio Fonseca <samfmf@gmail.com>
2019-04-17 14:12:19 -0300

    Removed unnecessary Singletons

--- Cappta.Logging/JsonLoggerProvider.cs
+++ Cappta.Logging/JsonLoggerProvider.cs
@@ -7,28 +7,18 @@ namespace Cappta.Logging
 {
 	public class JsonLoggerProvider : ILoggerProvider, ISupportExternalScope
 	{
-		private bool configured = false;
-		private ILogConverter logConverter;
-		private ILogService logService;
+		private readonly ILogConverter logConverter;
+		private readonly ILogService logService;
 		private IExternalScopeProvider scopeProvider;
 
-		public static JsonLoggerProvider Instance { get; } = new JsonLoggerProvider();
-
-		public void Configure(ILogConverter logConverter, ILogService logService)
+		public JsonLoggerProvider(ILogConverter logConverter, ILogService logService)
 		{
-			if (this.configured) { throw new InvalidOperationException($"Cannot configure {nameof(JsonLoggerProvider)} again"); }
-
 			this.logConverter = logConverter ?? throw new ArgumentNullException(nameof(logConverter));
 			this.logService = logService ?? throw new ArgumentNullException(nameof(logService));
-			this.configured = true;
 		}
 
 		public ILogger CreateLogger(string categoryName)
-		{
-			if (this.configured == false) { throw new InvalidOperationException($"{nameof(JsonLoggerProvider)} has not been configured yet"); }
-
-			return new JsonLogger(categoryName, this.logConverter, this.logService, this.scopeProvider);
-		}
+			=> new JsonLogger(categoryName, this.logConverter, this.logService, this.scopeProvider);
 
 		public void Dispose() { }
 
