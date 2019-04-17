c155feba2fe1a2a0832faa58adfa5d8b80372751
Sérgio Fonseca <samfmf@gmail.com>
2019-04-17 14:12:55 -0300

    Improved string  ToCamelCase to detect if any change is required

--- Cappta.Logging/Extensions/StringExtensions.cs
+++ Cappta.Logging/Extensions/StringExtensions.cs
@@ -1,8 +1,12 @@
-﻿namespace Cappta.Logging.Extensions
+namespace Cappta.Logging.Extensions
 {
 	internal static class StringExtensions
 	{
 		public static string ToCamelCase(this string value)
-			=> $"{char.ToLower(value[0])}{value.Substring(1)}";
+		{
+			if(char.IsLower(value[0])) { return value; }
+
+			return char.ToLower(value[0]) + value.Substring(1);
+		}
 	}
 }
