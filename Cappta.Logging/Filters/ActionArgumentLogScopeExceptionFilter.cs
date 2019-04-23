2fbb2343b737fc26787c5511cb877794b591833e
Sérgio Fonseca <sergio.fonseca@cappta.com.br>
2019-04-23 11:21:31 -0300

    Fixed incompatibility with FromBody ActionArgument

--- Cappta.Logging/Filters/ActionArgumentLogScopeExceptionFilter.cs
+++ Cappta.Logging/Filters/ActionArgumentLogScopeExceptionFilter.cs
@@ -1,22 +1,31 @@
-﻿using Cappta.Logging.Extensions;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Cappta.Logging.Filters
 {
-	public abstract class ActionArgumentLogScopeExceptionFilter : IExceptionFilter
+	public abstract class ActionArgumentLogScopeExceptionFilter : IExceptionFilter, IAsyncActionFilter
 	{
+		private IDictionary<string, object> actionArguments;
+
 		private readonly ILogger<ActionArgumentLogScopeExceptionFilter> logger;
 
 		public ActionArgumentLogScopeExceptionFilter(ILogger<ActionArgumentLogScopeExceptionFilter> logger)
-			=> this.logger = logger ?? throw new System.ArgumentNullException(nameof(logger));
+			=> this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+		public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+		{
+			this.actionArguments = context.ActionArguments;
+			await next();
+		}
 
 		public void OnException(ExceptionContext context)
 		{
 			var state = new SortedDictionary<string, object>() {
-				{ "ActionArgument", context.ModelState.ToRawValueDictionary() }
+				{ "ActionArgument", this.actionArguments }
 			};
 			using (this.logger.BeginScope(state))
 			{
