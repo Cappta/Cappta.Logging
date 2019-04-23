e50f626d2d0d1095748052c23c5b6438978277be
Renato de Miranda Rios <renato.rios@cappta.com.br>
2019-04-23 18:18:12 -0300

    Includes IRestResponse convert

--- Cappta.Logging/Converters/MainObjectConverter.cs
+++ Cappta.Logging/Converters/MainObjectConverter.cs
@@ -1,5 +1,6 @@
 ﻿using Cappta.Logging.Extensions;
 using Microsoft.Extensions.Logging.Internal;
+using RestSharp;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -37,10 +38,25 @@ namespace Cappta.Logging.Converters
 				case AggregateException aggregateException: return this.ConvertAggregateException(aggregateException, logSerializer);
 				case Exception ex: return this.ConvertException(ex, logSerializer);
 				case IEnumerable enumerable: return this.ConvertEnumerable(enumerable, logSerializer);
+				case IRestResponse restResponse: return this.ConvertIRestResponse(restResponse, logSerializer);
 				default:
 					if (obj.GetType().IsPrimitive) { return obj; }
 					return this.Reflect(obj, logSerializer);
 			}
+		}
+
+		private object ConvertAggregateException(AggregateException aggregateException, ILogConverter logSerializer)
+		{
+			var dict = new SortedDictionary<string, object>() {
+				{ "StackTrace", aggregateException.StackTrace },
+				{ "Type", aggregateException.GetType().FullName },
+				{ "InnerException", logSerializer.ConvertToLogObject(aggregateException.InnerException) },
+			};
+			for (var i = 1; i < aggregateException.InnerExceptions.Count; i++)
+			{
+				dict.Add($"InnerException{i + 1}", logSerializer.ConvertToLogObject(aggregateException.InnerExceptions[i]));
+			}
+			return dict;
 		}
 
 		private object ConvertEnumerable(IEnumerable enumerable, ILogConverter logSerializer)
@@ -50,6 +66,45 @@ namespace Cappta.Logging.Converters
 			return new SortedDictionary<string, object>() { { "Values", logObjects.ToArray() } };
 		}
 
+		private object ConvertException(Exception ex, ILogConverter logSerializer)
+		{
+			var dict = new SortedDictionary<string, object>() {
+				{ "Message", ex.Message },
+				{ "StackTrace", ex.StackTrace },
+				{ "Type", ex.GetType().FullName }
+			};
+			if (ex.InnerException != null) { dict.Add("InnerException", logSerializer.ConvertToLogObject(ex.InnerException)); }
+			return dict;
+		}
+
+		private object ConvertFormattedLogValues(FormattedLogValues formattedLogValues, ILogConverter logSerializer)
+		{
+			var dict = new SortedDictionary<string, object>() {
+				{ "Message", formattedLogValues.ToString() }
+			};
+
+			foreach (var kvp in formattedLogValues)
+			{
+				if (kvp.Key == OriginalFormatKey) { continue; }
+
+				dict.Add(kvp.Key, kvp.Value);
+			}
+
+			return dict;
+		}
+
+		private object ConvertIRestResponse(IRestResponse restResponse, ILogConverter logSerializer)
+		{
+			var dict = new SortedDictionary<string, object>()
+			{
+				{ "Status", restResponse.StatusCode },
+				{ "StatusCode", (int)restResponse.StatusCode },
+				{ "Content", restResponse.Content },
+				{ "State", restResponse.ErrorMessage },
+				{ "Exception", logSerializer.ConvertToLogObject(restResponse.ErrorException) }
+			};
+			return dict;
+		}
 		private object ConvertKvpEnumerable(IEnumerable<KeyValuePair<string, object>> kvpEnumerable, ILogConverter logSerializer)
 		{
 			var kvpListDict = new SortedDictionary<string, object>();
@@ -86,48 +141,6 @@ namespace Cappta.Logging.Converters
 			if (string.IsNullOrWhiteSpace(thread.Name) == false) { dict.Add("Name", thread.Name); }
 			return dict;
 		}
-
-		private object ConvertFormattedLogValues(FormattedLogValues formattedLogValues, ILogConverter logSerializer)
-		{
-			var dict = new SortedDictionary<string, object>() {
-				{ "Message", formattedLogValues.ToString() }
-			};
-
-			foreach (var kvp in formattedLogValues)
-			{
-				if (kvp.Key == OriginalFormatKey) { continue; }
-
-				dict.Add(kvp.Key, kvp.Value);
-			}
-
-			return dict;
-		}
-
-		private object ConvertAggregateException(AggregateException aggregateException, ILogConverter logSerializer)
-		{
-			var dict = new SortedDictionary<string, object>() {
-				{ "StackTrace", aggregateException.StackTrace },
-				{ "Type", aggregateException.GetType().FullName },
-				{ "InnerException", logSerializer.ConvertToLogObject(aggregateException.InnerException) },
-			};
-			for (var i = 1; i < aggregateException.InnerExceptions.Count; i++)
-			{
-				dict.Add($"InnerException{i + 1}", logSerializer.ConvertToLogObject(aggregateException.InnerExceptions[i]));
-			}
-			return dict;
-		}
-
-		private object ConvertException(Exception ex, ILogConverter logSerializer)
-		{
-			var dict = new SortedDictionary<string, object>() {
-				{ "Message", ex.Message },
-				{ "StackTrace", ex.StackTrace },
-				{ "Type", ex.GetType().FullName }
-			};
-			if (ex.InnerException != null) { dict.Add("InnerException", logSerializer.ConvertToLogObject(ex.InnerException)); }
-			return dict;
-		}
-
 		private object Reflect(object obj, ILogConverter logSerializer)
 		{
 			var dict = new SortedDictionary<string, object>();
