73c91a717b1fef1bff57f2c38dda9816093bf49e
SammyROCK <samfmf@gmail.com>
2020-10-14 11:54:53 -0300

    Created AuthorizedClientIdLogScopeFilter

---
+++ Cappta.Logging/Filters/AuthorizedClientIdLogScopeFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Cappta.Logging.Filters
+{
+	public class AuthorizedClientIdLogScopeFilter : IAsyncActionFilter
+	{
+		private readonly ILogger<AuthorizedClientIdLogScopeFilter> logger;
+
+		public AuthorizedClientIdLogScopeFilter(ILogger<AuthorizedClientIdLogScopeFilter> logger)
+			=> this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+		public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+		{
+			var claimsPrincipal = context.HttpContext.User;
+			var clientIdClaim = claimsPrincipal?.FindFirst("client_id");
+			var clientId = clientIdClaim?.Value;
+
+			var state = new SortedDictionary<string, object>(StringComparer.OrdinalIgnoreCase) {
+				{ "AuthorizedClientId", clientId }
+			};
+			using (this.logger.BeginScope(state))
+			{
+				await next();
+			}
+		}
+	}
+}
