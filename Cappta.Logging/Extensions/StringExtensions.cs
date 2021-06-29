9adc92bf81e251a4b442b3332d3b8821280d8fdd
SammyROCK <samfmf@gmail.com>
2021-06-29 11:20:23 -0300

    Using range operator

--- Cappta.Logging/Extensions/StringExtensions.cs
+++ Cappta.Logging/Extensions/StringExtensions.cs
@@ -3,13 +3,13 @@ namespace Cappta.Logging.Extensions {
 		public static string ToCamelCase(this string value) {
 			if(char.IsLower(value[0])) { return value; }
 
-			return char.ToLower(value[0]) + value.Substring(1);
+			return char.ToLower(value[0]) + value[1..];
 		}
 
 		public static string ToPascalCase(this string value) {
 			if(char.IsUpper(value[0])) { return value; }
 
-			return char.ToUpper(value[0]) + value.Substring(1);
+			return char.ToUpper(value[0]) + value[1..];
 		}
 	}
 }
