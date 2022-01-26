ce25443e782ffc5a852fe6af2a4f24097be8db03
SammyROCK <samfmf@gmail.com>
2022-01-26 11:47:36 -0300

    Fixed RestSharp JsonBody

--- Cappta.Logging/Extensions/IRestRequestExtensions.cs
+++ Cappta.Logging/Extensions/IRestRequestExtensions.cs
@@ -3,6 +3,6 @@ using RestSharp;
 namespace Cappta.Logging.Extensions {
 	internal static class RestRequestExtensions {
 		public static void AddRawJsonBody(this RestRequest restRequest, string rawJson)
-			=> restRequest.AddParameter("application/json", rawJson, ParameterType.RequestBody);
+			=> restRequest.AddStringBody(rawJson, DataFormat.Json);
 	}
 }
